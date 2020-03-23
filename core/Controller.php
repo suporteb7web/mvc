4a989dfcf973314839de650ebd6cdff16c5525cf
Bonieky Lacerda <suporte@b7web.com.br>
2020-03-23 13:51:48 -0300

    base funcionality

--- core/Controller.php
+++ core/Controller.php
@@ -1,12 +1,26 @@
 <?php
 namespace core;
 
+use \src\Config;
+
 class Controller {
+
+    private function getBaseUrl() {
+        $base = (isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on') ? 'https://' : 'http://';
+        $base .= $_SERVER['SERVER_NAME'];
+        if($_SERVER['SERVER_PORT'] != '80') {
+            $base .= ':'.$_SERVER['SERVER_PORT'];
+        }
+        $base .= Config::BASE_DIR;
+        
+        return $base;
+    }
 
     private function _render($folder, $viewName, $viewData = []) {
         if(file_exists('../src/views/'.$folder.'/'.$viewName.'.php')) {
             extract($viewData);
             $render = fn($vN, $vD = []) => $this->renderPartial($vN, $vD);
+            $base = $this->getBaseUrl();
             require '../src/views/'.$folder.'/'.$viewName.'.php';
         }
     }
